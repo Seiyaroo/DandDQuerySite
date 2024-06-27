f3141d8c782563b72cc1c8f5f41e96b1568df5be
Nicklas Di Pietro <37163331+Seiyaroo@users.noreply.github.com>
2024-06-27 14:47:51 -0700

    Initialization
    Initial Setup of Project

---
+++ src/SpellCard.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { getAllSpells } from "./api";
+import SpellCard from "./SpellCard";
+import "./styles.css";
+
+export default function App() {
+    const [spells, setSpells] = useState([]);
+
+    useEffect(() => {
+        const savedSpells = localStorage.getItem("spells");
+        if (savedSpells) setSpells(JSON.parse(savedSpells));
+        getAllSpells().then((spells) => {
+            setSpells(spells);
+            localStorage.setItem("spells", JSON.stringify(spells));
+        });
+    }, []);
+
+    // Import image into spellcard somewhere in here down the road.
+    // AWS Bucket probably / AZURE / GCP
+    return (
+        <div className="App">
+            {spells.length === 0 && <span className="loading">Loading...</span>}
+            <ul className="spell-list">
+                {spells.map((spell) => (
+                    <SpellCard key={spell.index} spell={spell} />
+                ))}
+            </ul>
+        </div>
+    );
+}
