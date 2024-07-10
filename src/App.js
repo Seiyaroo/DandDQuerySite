7364d710debdd4d0f62e0c4217a61c9acf671a34
Nicklas Di Pietro <37163331+Seiyaroo@users.noreply.github.com>
2024-07-10 16:47:05 -0700

    Testing search functionality set up.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import {getAllMonsters, getAllSpells} from "./api";
-import SpellCard from "./SpellCard";
-import MonsterCard from "./MonsterCard";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SearchComponent from "./components/SearchComponent";
+import SpellCard from "./components/SpellCard";
+import MonsterCard from "./components/MonsterCard";
 import "./styles.css";
 
 export default function App(effect, deps) {
@@ -27,21 +29,87 @@ export default function App(effect, deps) {
       })
     }, []);
 
-  return (
-      <div className="App">
-        {spells.length === 0 && <span className="loading">Loading...</span>}
-          <ul className="spell-list">
-          {spells.map((spell) => (
-              <SpellCard key={spell.index} spell={spell} />
-          ))}
-        </ul>
-          {monsters.length === 0 && <span className="loading">Loading...</span> }
-          <ul className="monster-list">
-              {monsters.map((monster) => (
-                  <MonsterCard key={monster.index} monster={monster} />
-              ))}
-          </ul>
-      </div>
-  );
+
+    return (
+        <div className="App">
+            <Router>
+                <header className="App-header">
+                    <h1>D&D Compendium</h1>
+                    {/* Add SearchComponent here */}
+                    <SearchComponent />
+                </header>
+                <main>
+                    <Routes>
+                        {/* Define routes for different components */}
+                        <Route
+                            path="/spells"
+                            element={
+                                <div>
+                                    {spells.length === 0 && <span className="loading">Loading...</span>}
+                                    <ul className="spell-list">
+                                        {spells.map((spell) => (
+                                            <SpellCard key={spell.index} spell={spell} />
+                                        ))}
+                                    </ul>
+                                </div>
+                            }
+                        />
+                        <Route
+                            path="/monsters"
+                            element={
+                                <div>
+                                    {monsters.length === 0 && <span className="loading">Loading...</span>}
+                                    <ul className="monster-list">
+                                        {monsters.map((monster) => (
+                                            <MonsterCard key={monster.index} monster={monster} />
+                                        ))}
+                                    </ul>
+                                </div>
+                            }
+                        />
+                        <Route
+                            path="/"
+                            element={<div>Welcome to the Compendium</div>}
+                        />
+                    </Routes>
+                </main>
+            </Router>
+        </div>
+    );
 }
+
+//   return (
+//       <div className="App">
+//           <Router>
+//               <header className={"App-header"}>
+//                   <h1>My Search App</h1>
+//                   <SearchComponent></SearchComponent>
+//               </header>
+//               <main>
+//                   <Routes>
+//                       <Route
+//                           path="/spells"
+//                           element={
+//                             <div>
+//
+//                             </div>
+//                           }
+//                   </Routes>
+//               </main>
+//           </Router>
+//         {spells.length === 0 && <span className="loading">Loading...</span>}
+//           <ul className="spell-list">
+//           {spells.map((spell) => (
+//               <SpellCard key={spell.index} spell={spell} />
+//           ))}
+//         </ul>
+//           {monsters.length === 0 && <span className="loading">Loading...</span> }
+//           <ul className="monster-list">
+//               {monsters.map((monster) => (
+//                   <MonsterCard key={monster.index} monster={monster} />
+//               ))}
+//           </ul>
+//       </div>
+//   );
+// }
 
